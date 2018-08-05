bad2e3b33d78188d906ccbbb679ade9ce3153339
nadhem <nadhem@nadhem-PC.mshome.net>
2018-08-05 09:55:56 +0200

    .

--- testPath/src/main/java/testPath/Test.java
+++ testPath/src/main/java/testPath/Test.java
@@ -1,5 +1,11 @@
 package testPath;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.util.StringTokenizer;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -8,9 +14,36 @@ import javafx.stage.Stage;
 public class Test extends Application {
 
 	public static void main(String[] args) {
-		System.out.println(System.getProperty("sun.arch.data.model"));
-		System.out.println(System.getProperty("java.version"));
-		launch(args);
+
+
+		// System.out.println(System.getProperty("sun.arch.data.model"));
+		// System.out.println(System.getProperty("java.version"));
+
+		String property = System.getProperty("java.library.path");
+		StringTokenizer parser = new StringTokenizer(property, ";");
+		while (parser.hasMoreTokens()) {
+			System.out.println(parser.nextToken());
+		}
+		Connection con = null;
+		try {
+			//DriverManager.registerDriver(new SQLServerDriver());
+
+			con = DriverManager.getConnection("jdbc:sqlserver://NADHEM-PC\\SQLEXPRESS;integratedSecurity=true;");
+
+			DatabaseMetaData meta = con.getMetaData();
+			ResultSet res = meta.getCatalogs();
+			System.out.println("List of databases: ");
+			while (res.next()) {
+				System.out.println(" " + res.getString("TABLE_CAT"));
+			}
+			res.close();
+
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("done");
+		// launch(args);
 	}
 
 	@Override
